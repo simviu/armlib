1a4d1a09828d9e02233c1c30a2e2474cfe9a7167
wuchangji <changji.wu@elephantrobotics.com>
2022-12-13 23:19:48 +0800

    [mycobot]: pick place test2

--- py/mycobot/test/t_pick_place2.py
+++ py/mycobot/test/t_pick_place2.py
@@ -31,21 +31,26 @@ def test(mycobot):
 
     set_grip(mycobot, False)
     time.sleep(1)
-    mycobot.send_coords([-110, -200, 140,  85, 40, 0  ], sp, 0)
-    time.sleep(1)
-    mycobot.send_coords([-110, -200, 80,  85, 40, 0], sp, 0)
-    time.sleep(1)
+    mycobot.send_coords([-110, -200, 160,  85, 40, 0  ], sp, 0)
+    time.sleep(3)
+    mycobot.send_coords([-170, -200, 140,  85, 40, -10], sp, 0)
+    time.sleep(5)
+    mycobot.send_coords([-173, -190, 80,  70, 43, -17], sp, 0)
+    time.sleep(5)
     set_grip(mycobot, True)
-    time.sleep(1)
-    mycobot.send_coords([ 110, -200, 140,  85, 40, 0  ], sp, 0)
-    time.sleep(1)
-
+    time.sleep(2)
+    mycobot.send_coords([-110, -200, 160,  85, 40, 0], sp, 0)
+    time.sleep(2)
     mycobot.send_coords([ 110, -200, 160,  85, 40, 0  ], sp, 0)
-    time.sleep(1)
+    time.sleep(3)
+
+    mycobot.send_coords([ 120, -220, 60,  170, -5, -110  ], sp, 0)
+
+    time.sleep(3)
     set_grip(mycobot, False)
     time.sleep(1)
 
-    mycobot.send_coords([ 110, -200, 160,  85, 40, 0  ], sp, 0)
+    mycobot.send_coords([ 110, -160, 160,  85, 40, 0   ], sp, 0)
     time.sleep(1)
 
 
