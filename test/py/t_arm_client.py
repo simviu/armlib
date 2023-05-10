3d8fbdb5a965fcc2316117781f73ce77209df77b
Sherman Chen <schen@simviu.com>
2023-05-10 22:34:25 +0800

    [ros]: python t_arm_client.py ok with moveTo and setJoints.

--- test/py/t_arm_client.py
+++ test/py/t_arm_client.py
@@ -8,21 +8,33 @@ HOST, PORT = "localhost", 8192
 # Create a socket (SOCK_STREAM means a TCP socket)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 if sock is None:
-    print("Socket failed")
+    print("Socket failed, please start armcmd server at port 8192")
     exit(1)
 
 print("Socket client connected to:"+HOST+":"+str(PORT))
 # Connect to server and send data
 sock.connect((HOST, PORT))
 
+#-----
+def send_cmd(scmd):
 
-scmd = "moveto xyz=-0.3,-0.1,0.4 rvec=180,0,0 spd=1 grip=-0.3"
-scmd += "\n"
-print("sending :'"+scmd+"'")
-sock.sendall(bytes(scmd, "utf-8"))
+    print("sending :'"+scmd+"'")
+    scmd1 = scmd + "\n"
+    sock.sendall(bytes(scmd1, "utf-8"))
+    # Receive data from the server and shut down
+    s = str(sock.recv(1024), "utf-8")
+    print("recv:"+s)
 
-# Receive data from the server and shut down
-s = str(sock.recv(1024), "utf-8")
-print("recv:"+s)
+
+#---- main
+send_cmd("init arm=z1")
+time.sleep(2)
+
+send_cmd("moveto xyz=0.2,0,0.3")
+time.sleep(5)
+send_cmd("st")
+
+send_cmd("setJoints angles=30,10,-20,20,20,-15 grip=1 t=2")
+send_cmd("st")
 
 time.sleep(5)
