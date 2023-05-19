10ba4f8cf7a96276ab72e39c6440b2acd2afce13
Sherman Chen <schen@simviu.com>
2023-05-19 11:51:17 +0800

    py slightly

--- py/pyarmlib/armTcp.py
+++ py/pyarmlib/armTcp.py
@@ -6,6 +6,7 @@ import time
 HOST = "127.0.0.1" 
 PORT = 8192  
 
+LN_MAX_CHARS = 1024
 
 #-------------
 # ArmTcp
@@ -14,6 +15,18 @@ class ArmTcp:
     def __init__(self, sName):
         self.sName = sName
 
+    #----
+    def recvLn(self):
+        s = ""
+        while True:
+            c = str(sock.recv(1))
+            if c == "\n":
+                return s
+            print("r:"+c)
+            s = s + c
+            
+            
+        
     #----    
     def connect(self, sHost=HOST, port=PORT):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -25,6 +38,11 @@ class ArmTcp:
     
         scmd = "init arm=z1"
         sock.sendall(bytes(scmd, "utf-8"))
+        print("cmd sent:"+scmd)
+        time.sleep(1)      
+        
+        sLn = self.recvLn()
+        print("Recv ln:"+sLn)  
         time.sleep(1)
         
        
