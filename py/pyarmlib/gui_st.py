e1fb32779e3d783c22efd0f3fbbadc45d2145dc9
Sherman Chen <schen@simviu.com>
2023-06-16 14:28:53 +0800

    [py]: coding gui_st...

--- py/pyarmlib/gui_st.py
+++ py/pyarmlib/gui_st.py
@@ -1,39 +1,50 @@
 import tkinter as tk
 from tkinter import ttk
-from armTcp import *
+from armLib import *
 from utils import *
 import threading
 
-
+BORDER_W = 2
 #----------
-class StPanel:
-    def __init__(self, topFrm, arm):
+class StPanel(object):
+    def __init__(self, topFrm, arm, N_joints):
         self.arm_ = arm
+        self.N_joints = N_joints
 
         #----
         frm = ttk.Frame(topFrm, padding=(3,3,12,12))
-        ln = tk.Label(frm, text = "joint"+str(idx+1))
-        ls = tk.Label(frm, text = "q,qd,Torque")
-        la = tk.Label(frm, text = "angle")
+        self.label_joints_ = []
+        for i in range(N_joints):
+            lt = tk.Label(frm, borderwidth = BORDER_W, text = "J"+str(i+1))
+            lv = tk.Label(frm, borderwidth = BORDER_W, text = "0.0")
+            lt.grid(column=i, row=0, sticky=(tk.N, tk.S, tk.E, tk.W))
+            lv.grid(column=i, row=1, sticky=(tk.N, tk.S, tk.E, tk.W))
+            self.label_joints_.append(lv)
 
-        bar=tk.Scrollbar(frm, orient='horizontal')
-        #bar.set(0.30, 0.5)
-        bar['command'] = self.scrollCbk
-
-        ln.grid(row=1, column=0, sticky=(tk.E,tk.W))
-        bar.grid(row=1, column=1, sticky=(tk.E,tk.W))
-        ls.grid(row=0, column=2, sticky=(tk.E,tk.W))
-        la.grid(row=0, column=1, sticky=(tk.E,tk.W))
-
-        frm.columnconfigure(1, minsize=500)
-        frm.rowconfigure(1, minsize=50)
-        frm.grid(row=idx, column=0, sticky=(tk.N,tk.S,tk.W,tk.E))
         self.frm = frm
+        return 
 
-        self.bar = bar 
-        self.frm = frm
-        self.label_st = ls
-        self.label_angle = la
-        #----
-        self.update()
+    #---
+    def update(self):
+        st = self.arm_.getSt()
+
         return
+
+#------------------
+class TestApp:
+    def __init__(self, root):
+        arm = Arm()
+        frm = ttk.Frame(root, padding=(3,3,12,12))
+        frm.grid(column=0, row=0, sticky=(tk.N, tk.S, tk.E, tk.W))
+
+        self.pnl_ = StPanel(frm, arm, 6)
+        self.pnl_.frm.grid(column=0, row=0, sticky=(tk.N, tk.S, tk.E, tk.W))
+        self.frm = frm
+
+#----------
+# main
+#----------
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = TestApp(root)
+    root.mainloop()
--- py/pyarmlib/test.py
+++ py/pyarmlib/test.py
@@ -1,4 +1,5 @@
 import tkinter as tk
 from tkinter import ttk
 from armTcp import *
+from gui_tip import TipPanel
 
