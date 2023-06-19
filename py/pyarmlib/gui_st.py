40e08d74bdbb12f929944fb1a27d064c0e95ef26
Sherman Chen <schen@simviu.com>
2023-06-19 08:10:48 +0800

    [py]: gui_st passive and fix.

--- py/pyarmlib/gui_st.py
+++ py/pyarmlib/gui_st.py
@@ -5,10 +5,17 @@ from utils import *
 import threading
 
 BORDER_W = 2
+
 #----------
-class StPanel(object):
-    def __init__(self, topFrm, arm, N_joints):
+class StTipPanel(object):
+    def __init__(self, topFrm, arm):
         self.arm_ = arm
+        frm = ttk.Frame(topFrm, padding=(3,3,12,12))
+
+
+#----------
+class StJointsPanel(object):
+    def __init__(self, topFrm, N_joints):
         self.N_joints = N_joints
 
         #----
@@ -16,37 +23,39 @@ class StPanel(object):
         self.label_joints_ = []
         for i in range(N_joints):
             lt = tk.Label(frm, borderwidth=BORDER_W, relief="solid", text = "J"+str(i+1))
-            lv = tk.Label(frm, borderwidth=BORDER_W, relief="solid", text = "0.0")
+            lv = tk.Label(frm, borderwidth=BORDER_W, relief="solid", text = "0.0", fg='blue')
             lt.grid(column=i, row=0, sticky=(tk.N, tk.S, tk.E, tk.W))
             lv.grid(column=i, row=1, sticky=(tk.N, tk.S, tk.E, tk.W))
+            self.label_joints_.append(lv)
             frm.columnconfigure(i, minsize=100)
 
         for i in range(2):
             frm.rowconfigure(i, minsize=50)
 
-            self.label_joints_.append(lv)
-
         self.frm = frm
         return 
 
     #---
-    def update(self):
-        st = self.arm_.getSt()
+    def set(self, joints):
+        for i in range(self.N_joints):
+            self.label_joints_[i].config(text=str(joints[i]))
 
         return
 
 #------------------
 class TestApp:
     def __init__(self, root):
-        arm = Arm()
         frm = ttk.Frame(root, padding=(3,3,12,12))
         frm.grid(column=0, row=0, sticky=(tk.N, tk.S, tk.E, tk.W))
 
         lTitle = tk.Label(frm, borderwidth = BORDER_W, text = "Test")
         lTitle.grid(column=0, row=0, sticky=(tk.N, tk.S, tk.E, tk.W))
-        self.pnl_ = StPanel(frm, arm, 6)
+        self.pnl_ = StJointsPanel(frm, 6)
         self.pnl_.frm.grid(column=0, row=1, sticky=(tk.N, tk.S, tk.E, tk.W))
         self.frm = frm
+
+        #---- set
+        self.pnl_.set([10,20,30,40,50,60])
 
 #----------
 # main
